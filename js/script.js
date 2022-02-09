e14cbd5652afa087e1ee73554aa4b0d952ba375e
EvgeniyaDanilovich <evgeniyadanilovich@gmail.com>
2022-02-10 00:12:02 +0300

    feat: create project galery-API

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const inputValue = document.querySelector('.search__input');
+const btn = document.querySelector('.search__btn');
+let tmp = 'search';
+
+
+let url = `https://api.unsplash.com/search/photos?query=${tmp}&client_id=yp-8LChNIuFJyZE7IyfPg4KGxoNQGkbZmO11z7avuRE`;
+const imgContainer = document.querySelector('.search__container');
+
+const showData = (data) => {
+
+	if (imgContainer.hasChildNodes()) {
+		imgContainer.innerHTML = '';
+	}
+
+	for (let i = 0; i <= 9; i++) {
+		let url = data['results'][i]['urls']['regular'];
+		const img = `<img class="search__img" src="${url}" alt='image'>`;
+		imgContainer.insertAdjacentHTML('beforeend', img);
+	}
+}
+
+async function getData(link) {
+	const res = await fetch(link);
+	const data = await res.json();
+	showData(data);
+}
+getData(url);
+
+
+btn.addEventListener('click', function (e) {
+	e.preventDefault();
+	tmp = inputValue.value;
+	url = `https://api.unsplash.com/search/photos?query=${tmp}&client_id=yp-8LChNIuFJyZE7IyfPg4KGxoNQGkbZmO11z7avuRE`;
+	let link = url;
+	getData(link);
+})
+
+//======================================
+
